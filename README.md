2afff1d98b8320a1483bd744ba7012e3747ada1f
Hill <hill@kimo.com>
2015-07-14 15:04:32 +0400

    first

---
+++ README.md
@@ -0,0 +1,9 @@
+PMVC integrate with react.js demo code 
+===============
+
+## 1. git clone the source code
+
+## 2. use composer for install
+   * composer install
+
+## 3. link web server to htdocs/index.php
---
+++ htdocs/index.php
@@ -0,0 +1,25 @@
+<?php
+include_once('../vendor/autoload.php');
+PMVC\Load::mvc();
+use PMVC\ActionController as mvc;
+
+
+$options = array(
+   _PLUGIN=>array(
+        'dispatcher'=>null
+        ,'error'=>null
+        ,'debug'=>null
+        ,'dotenv'=>array('envfile'=>'../.env')
+        ,'routing'=>null
+    )
+    ,_ERROR_REPORTING=>E_ALL
+);
+
+
+
+$controller = new mvc($options);
+if($controller->plugApp()){
+    $controller->process();
+}
+
+

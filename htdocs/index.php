865a9fe683a066d6b7da3d48aa2d095fec269c04
Hill <hill@kimo.com>
2016-04-25 09:44:40 +0000

    integrate with pmvc7

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,22 +1,14 @@
 <?php
 include_once('../vendor/autoload.php');
-PMVC\Load::mvc();
-use PMVC\ActionController as mvc;
-
-$options = array(
-   _PLUGIN=>array(
-        'dispatcher'=>null
-        ,'error'=>null
-        ,'debug'=>null
-        ,'dotenv'=>array('envFile'=>'../.env')
-        ,'routing'=>null
-    )
-    ,_ERROR_REPORTING=>E_ALL
-);
-
-$controller = new mvc($options);
+\PMVC\Load::plug([
+    'controller'=>null
+    ,'dispatcher'=>null
+    ,'error'=>null
+    ,'debug'=>null
+    ,'dotenv'=>array('envFile'=>'../.env')
+    ,'app_action_router'=>null
+]);
+$controller = \PMVC\plug('controller',[_ERROR_REPORTING=>E_ALL]);
 if($controller->plugApp()){
-    $controller();
+    $controller->process();
 }
-
-
--- test.php
+++ test.php
@@ -1,29 +1,26 @@
 <?php
-\PMVC\Load::mvc();
+\PMVC\Load::plug();
 class ReactAppTest extends PHPUnit_Framework_TestCase
 {
     function testApp()
     {
-        $options = array(
-           _PLUGIN=>array(
-                'dispatcher'=>null
-                ,'error'=>null
-                ,'debug'=>null
-                ,'dotenv'=>array('envFile'=>'.env.sample')
-                ,'routing'=>null
-            )
-            ,_ERROR_REPORTING=>E_ALL
-        );
-
-        $controller = new \PMVC\ActionController($options);
-        $controller->store(array(
-            _RUN_PARENT=>'vendor/pmvc-app'
-            ,_TEMPLATE_DIR=>'vendor/pmvc-theme/hello_react'
+        \PMVC\initPlugin([
+            'controller'=>null
+            ,'dispatcher'=>null
+            ,'error'=>null
+            ,'debug'=>null
+            ,'dotenv'=>array('envFile'=>'.env.sample')
+            ,'app_action_router'=>null
+        ]);
+        $controller = \PMVC\plug('controller',[
+            _ERROR_REPORTING=>E_ALL
+            ,_RUN_PARENT=>'apps'
             ,'NODE'=>'vendor/bin/node'
-        ));
+            ,_TEMPLATE_DIR=>'vendor/pmvc-theme/hello_react'
+        ]);
         if($controller->plugApp()){
             ob_start();
-            $controller();
+            $controller->process();
             $output = ob_get_contents();
             ob_end_clean();
         }

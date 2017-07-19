0449a7668a472f3f5271bd06baeaa65604cb7026
Hill <hill@kimo.com>
2017-07-19 15:29:14 +0000

    update package for ttfb

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,15 +1,17 @@
 <?php
 
 include_once('../vendor/autoload.php');
+
 \PMVC\Load::plug([
     'controller'=>null
     ,'dispatcher'=>null
     ,'error'=>['all']
     ,'debug'=>null
     ,'dev'=>null
-    ,'dotenv'=>['../.env.sample']
+    ,'dotenv'=>[(is_file('../.env')? '../.env' : '../.env.sample')]
     ,'app_action_router'=>null
 ]);
+
 $controller = \PMVC\plug('controller');
 if($controller->plugApp()){
     $controller->process();

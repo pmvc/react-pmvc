b15cd8e4e8a83b263c37705e3a7796ed39c31e30
Hill <hill@kimo.com>
2017-09-13 12:43:00 +0000

    update hello-react

--- htdocs/index.php
+++ htdocs/index.php
@@ -10,6 +10,7 @@ include_once('../vendor/autoload.php');
     ,'dev'=>null
     ,'dotenv'=>[(is_file('../.env.pmvc')? '../.env.pmvc' : '../.env.sample')]
     ,'app_action_router'=>null
+    ,'view_config_helper'=>null
 ]);
 
 $controller = \PMVC\plug('controller');

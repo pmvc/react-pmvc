37c26fac66787382d2e24fc8f992c63abc626767
Hill <hill@kimo.com>
2021-05-01 03:49:14 +0800

    update config

--- clean.sh
+++ clean.sh
@@ -22,6 +22,11 @@ else
     sed -i -e "s|hello_app|${DIR}|g" .env.pmvc
     sed -i -e "s|/vendor/pmvc-theme/hello_react|/themes/${DIR}|" .env.pmvc
     if [ -e ".env.pmvc-e" ]; then rm .env.pmvc-e; fi
+    ln -sf ../themes/${DIR}/assets ./htdocs/assets
+    sed -i -e "s|\"pmvc-app\/hello_app\": \"\*\",||g" composer.json  
+    sed -i -e "s|\"pmvc-theme\/hello_react\": \"\*\"||g" composer.json  
+    sed -i -e "s|view_config_helper\": \"\*\",|view_config_helper\": \"\*\"|g" composer.json  
+    if [ -e "composer.json-e" ]; then rm composer.json-e; fi
     
     echo "clean vendor, git, composer.lock clean.sh"
     find ./vendor -type d | xargs rm -rf

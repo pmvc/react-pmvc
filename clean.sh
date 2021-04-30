3059740a2381fae16796b7e7bda12ea9220b7ad1
Hill <hill@kimo.com>
2021-05-01 03:06:23 +0800

    update clean.sh

--- clean.sh
+++ clean.sh
@@ -6,6 +6,18 @@ then
     echo "In react-pmvc origin folder will not process clean command"
 else
     echo "Start to clean folder [${DIR}]";
+
+    echo "copy app"
+    mkdir ./apps/$DIR
+    cp -a ./apps/link-to-see-demo/* ./apps/$DIR
+    rm ./apps/link-to-see-demo
+
+    echo "copy theme"
+    mkdir ./themes/$DIR
+    cp -a ./themes/link-to-see-demo/* ./themes/$DIR
+    rm ./themes/link-to-see-demo
+    
+    echo "clean vendor, git, composer.lock clean.sh"
     find ./vendor -type d | xargs rm -rf
     find ./.git -type d | xargs rm -rf
     find ./composer.lock | xargs rm -rf
